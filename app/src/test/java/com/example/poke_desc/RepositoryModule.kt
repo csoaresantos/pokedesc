a9d681111d0ef2596eb404c1c97827bd64e05117
Charles S <charlessoaresdossantos@gmail.com>
2021-01-31 21:17:47 -0300

    Imeplementatio of instrumental test of the entry screen

--- app/src/test/java/com/example/poke_desc/RepositoryModule.kt
+++
@@ -1,8 +0,0 @@
-package com.example.poke_desc
-
-import com.example.poke_desc.data.repository.PokemonRepository
-import org.koin.dsl.module
-
-val repositoryModule = module {
-    single { PokemonRepository(get()) }
-}
